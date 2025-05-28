102feeffb32dd0ecee95e4c91c0a76750118c260
jhowmodderx <heverttonrodrigo112@gmail.com>
2025-05-28 02:46:17 -0300

    Delete attached_assets directory

--- attached_assets/script.js
+++
@@ -1,9 +0,0 @@
-// script.js
-
-// Função para carregar usuários do localStorage
-function carregarUsuarios() {
-  const usuarios = localStorage.getItem('usuarios');
-  return usuarios ? JSON.parse(usuarios) : [];
-}
-
-// Função para
